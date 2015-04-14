5b7bfaa8482d8158fedbec82331f9a28b730518a
Javier Burguete <jburguete@eead.csic.es>
2015-04-14 12:38:34 +0200

    Providing more information on channel overflow

--- 1.3.8/flow_scheme.h
+++ 1.3.8/flow_scheme.h
@@ -395,6 +395,7 @@ static inline void _part_parameters2(int thread)
 			g_mutex_lock(mutex);
 			channel_overflow=1;
 			g_mutex_unlock(mutex);
+			printf("overflow in node %d: A="FWF" Amax="FWF"\n", i, A, Amax);
 			break;
 		}
 	}
